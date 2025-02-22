6ad89de87756b79871f6387b922d9a4fc7b2f0b3
Funkas <petrikasjulius@gmail.com>
2025-02-22 11:54:09 +0200

    create initial maven (empty) budget project

--- src/main/java/org/maven/jp_budget/App.java
+++ src/main/java/org/maven/jp_budget/App.java
@@ -1,8 +1,35 @@
 package org.maven.jp_budget;
 
-public class App {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
-    public static void main(String[]args){
+public class App {
+    static final BudgetService budgetService = new BudgetService();
+
+    public App() {
+    }
+
+    public static void main(String[] args) {
         System.out.println("Labas");
+        IncomeRecord incomeRecord = new IncomeRecord(BigDecimal.valueOf(1500L), "Salary", LocalDateTime.now(), true, (String)null);
+        ExpenseRecord expenseRecord = new ExpenseRecord(BigDecimal.valueOf(100L), "Food", LocalDateTime.now(), PaymentMethodType.CARD, new BankCard("Revolut", "1234"));
+        budgetService.setIncomeRecord(incomeRecord);
+        budgetService.setExpenseRecords(expenseRecord);
+        printIncomeRecords();
+        printExpenseRecords();
+    }
+
+    private static void printIncomeRecords() {
+        for(IncomeRecord record : budgetService.getIncomeRecords()) {
+            System.out.println(record);
+        }
+
+    }
+
+    private static void printExpenseRecords() {
+        for(ExpenseRecord record : budgetService.getExpenseRecords()) {
+            System.out.println(record);
+        }
+
     }
 }
